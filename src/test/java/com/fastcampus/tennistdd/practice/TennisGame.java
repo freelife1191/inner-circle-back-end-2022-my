af1cf43ccfc4912e932d48ab18dcd781ffc5f016
mskwon <mskwon@tidesquare.com>
2023-01-15 23:55:11 +0900

    (refactoring) rearrange code, extract method: isGreaterThanDeucePoints().

--- src/test/java/com/fastcampus/tennistdd/practice/TennisGame.java
+++ src/test/java/com/fastcampus/tennistdd/practice/TennisGame.java
@@ -34,19 +34,23 @@ class TennisGame {
         return TennisGameStatus.STARTED;
     }
 
-    private boolean isAdvantageOut() {
-        return 3 <= Math.min(serverPoints, receiverPoints) && 1 == receiverPoints - serverPoints;
-    }
-
-    private boolean isAdvantageIn() {
-        return 3 <= Math.min(serverPoints, receiverPoints) && 1 == serverPoints - receiverPoints;
+    private boolean isFinished() {
+        return 2 <= Math.abs(serverPoints - receiverPoints) && 4 <= Math.max(serverPoints, receiverPoints);
     }
 
     private boolean isDeuce() {
-        return 3 <= Math.min(serverPoints, receiverPoints) && serverPoints == receiverPoints;
+        return isGreaterThanDeucePoints() && serverPoints == receiverPoints;
     }
 
-    private boolean isFinished() {
-        return 2 <= Math.abs(serverPoints - receiverPoints) && 4 <= Math.max(serverPoints, receiverPoints);
+    private boolean isAdvantageIn() {
+        return isGreaterThanDeucePoints() && 1 == serverPoints - receiverPoints;
+    }
+
+    private boolean isAdvantageOut() {
+        return isGreaterThanDeucePoints() && 1 == receiverPoints - serverPoints;
+    }
+
+    private boolean isGreaterThanDeucePoints() {
+        return 3 <= Math.min(serverPoints, receiverPoints);
     }
 }
