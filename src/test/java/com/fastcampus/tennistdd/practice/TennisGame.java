c53c069b7fc93748d3f7c24036d275a80c37ce67
mskwon <mskwon@tidesquare.com>
2023-01-15 23:49:20 +0900

    (refactoring) extract method: isAdvantageIn().

--- src/test/java/com/fastcampus/tennistdd/practice/TennisGame.java
+++ src/test/java/com/fastcampus/tennistdd/practice/TennisGame.java
@@ -29,9 +29,13 @@ class TennisGame {
     public TennisGameStatus status() {
         if (isFinished()) return TennisGameStatus.FINISHED;
         if (isDeuce()) return TennisGameStatus.DEUCE;
-        if (3 <= Math.min(serverPoints, receiverPoints) && 1 == serverPoints - receiverPoints)
+        if (isAdvantageIn())
             return TennisGameStatus.ADVANTAGE_IN;
         return TennisGameStatus.STARTED;
+    }
+
+    private boolean isAdvantageIn() {
+        return 3 <= Math.min(serverPoints, receiverPoints) && 1 == serverPoints - receiverPoints;
     }
 
     private boolean isDeuce() {
