486250e0d21c51a5fffa52caf7f34e84a6c261a5
mskwon <mskwon@tidesquare.com>
2023-01-16 22:42:54 +0900

    (test passed) 0, -1, STARTED -> IllegalArgumentException.

--- src/test/java/com/fastcampus/tennistdd/practice/PresentTennisGameRequest.java
+++ src/test/java/com/fastcampus/tennistdd/practice/PresentTennisGameRequest.java
@@ -11,6 +11,7 @@ public final class PresentTennisGameRequest {
 
     public PresentTennisGameRequest(final int serverPoints, final int receiverPoints, final TennisGameStatus status) {
         Assert.isTrue(0 <= serverPoints, "serverPoints must not be a negative");
+        Assert.isTrue(0 <= receiverPoints, "receiverPoints must not be a negative");
         this.serverPoints = serverPoints;
         this.receiverPoints = receiverPoints;
         this.status = status;
