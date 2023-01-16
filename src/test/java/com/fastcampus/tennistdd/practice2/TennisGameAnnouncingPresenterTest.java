87a27302fb31be62ae3df8824d3d64ce76c70b5f
mskwon <mskwon@tidesquare.com>
2023-01-16 22:21:19 +0900

    add test class: TennisGameAnnouncingPresenter.

---
+++ src/test/java/com/fastcampus/tennistdd/practice2/TennisGameAnnouncingPresenterTest.java
@@ -0,0 +1,10 @@
+package com.fastcampus.tennistdd.practice2;
+
+import org.junit.jupiter.api.Test;
+
+public class TennisGameAnnouncingPresenterTest {
+
+    @Test
+    void case_deuce() {
+    }
+}
