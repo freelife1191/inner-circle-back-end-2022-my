ef09431dfa91470c43dbd4b0d9d9dd8af6d21cd0
mskwon <mskwon@tidesquare.com>
2023-01-16 23:07:07 +0900

    (test failed) 1, 2, STARTED -> fifteen thirty.

--- src/test/java/com/fastcampus/tennistdd/practice/TennisGameAnnouncingPresenterTest.java
+++ src/test/java/com/fastcampus/tennistdd/practice/TennisGameAnnouncingPresenterTest.java
@@ -60,6 +60,7 @@ public class TennisGameAnnouncingPresenterTest {
         assertStartedStatusResult(0, 0, "love all");
         assertStartedStatusResult(0, 1, "love fifteen");
         assertStartedStatusResult(1, 1, "fifteen all");
+        assertStartedStatusResult(1, 2, "fifteen thirty");
     }
 
     private void assertStartedStatusResult(final int serverPoints, final int receiverPoints, final String expected) {
